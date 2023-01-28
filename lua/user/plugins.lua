3a8a66d75d1d79672182ed86a3a5d12616df0108
Niku Grönberg <gronbergniku@gmail.com>
2023-01-28 11:33:44 +0200

    run TSUpdate automatically

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,7 +71,8 @@ return require("packer").startup(function(use)
 		use({
 			"nvim-treesitter/nvim-treesitter",
 			run = function()
-				require("nvim-treesitter.install").update({ with_sync = true })
+				local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+				ts_update()
 			end,
 		})
 		use("nvim-treesitter/nvim-treesitter-refactor")
