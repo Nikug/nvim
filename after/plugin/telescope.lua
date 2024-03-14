959cfe1b946bc424b921ae7d1ac4af43a83e5a2d
niku <niku.gronberg@develore.com>
2024-03-14 15:51:26 +0200

    sort last used buffer by

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -23,6 +23,9 @@ require("telescope").setup({
 		-- }
 		-- Now the picker_config_key will be applied every time you call this
 		-- builtin picker
+		buffers = {
+			sort_lastused = true,
+		},
 	},
 	extensions = {
 		-- Your extension configuration goes here:
