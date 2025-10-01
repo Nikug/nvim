9b4152b8621f4c8f2d3771695d24afc689427c3d
niku <niku.gronberg@develore.com>
2025-10-01 09:29:28 +0300

    add omnisharp extended keyboard mappings, update packages

--- after/plugin/mason-lspconfig.lua
+++ after/plugin/mason-lspconfig.lua
@@ -8,12 +8,44 @@ require("mason-lspconfig").setup({
 vim.diagnostic.config({ virtual_text = true, severity_sort = true })
 
 -- LSP Keybinds
+
+local function csharp_lsp_keymaps(buf)
+	vim.keymap.set(
+		"n",
+		"gd",
+		"<cmd>:lua require('omnisharp_extended').lsp_definition()<cr>",
+		{ buffer = buf, noremap = true }
+	)
+	vim.keymap.set(
+		"n",
+		"gi",
+		"<cmd>:lua require('omnisharp_extended').lsp_implementation()<cr>",
+		{ buffer = buf, noremap = true }
+	)
+	vim.keymap.set("n", "gr", function()
+		require("omnisharp_extended").telescope_lsp_references()
+	end, { buffer = buf, noremap = true })
+	vim.keymap.set(
+		"n",
+		"gt",
+		"<cmd>:lua require('omnisharp_extended').lsp_type_definition()<cr>",
+		{ buffer = buf, noremap = true }
+	)
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
-		vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { buffer = ev.buf })
-		vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", { buffer = ev.buf })
-		vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = ev.buf })
-		vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<cr>", { buffer = ev.buf })
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+		if client.name == "omnisharp" then
+			csharp_lsp_keymaps(ev.buf)
+		else
+			vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", { buffer = ev.buf })
+			vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", { buffer = ev.buf })
+			vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = ev.buf })
+			vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<cr>", { buffer = ev.buf })
+		end
+
 		vim.keymap.set("n", "gK", "<cmd>:lua vim.diagnostic.open_float()<cr>", { buffer = ev.buf })
 	end,
 })
@@ -42,12 +74,9 @@ vim.lsp.config("volar", {
 vim.lsp.config("omnisharp", {
 	settings = {
 		RoslynExtensionsOptions = {
-			EnableAnalyzersSupport = false,
+			EnableAnalyzersSupport = true,
 			EnableImportCompletion = true,
+			EnableDecompilationSupport = true,
 		},
-	},
-	handlers = {
-		["textDocument/definition"] = require("omnisharp_extended").definition_handler,
-		["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
 	},
 })
--- after/plugin/supermaven.lua
+++ after/plugin/supermaven.lua
@@ -1,6 +1,6 @@
 require("supermaven-nvim").setup({
 	keymaps = {
-		accept_suggestion = "<Tab>",
+		accept_suggestion = "<S-Tab>",
 	},
 	disable_inline_completion = false,
 	disable_keymaps = false,
